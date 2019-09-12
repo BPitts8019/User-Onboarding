3c414ff0419c0fc1ba3c60b27b779344e48e2299
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-12 16:50:07 -0700

    Added styles to form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {withFormik, Form, Field} from "formik";
 
 function UserForm (props) {
    return (
-      <div>A Form</div>
+      <Form>
+         <Field type="text" name="name" placeholder="Name" />
+         <Field type="email" name="email" placeholder="Email" />
+         <Field type="password" name="password" placeholder="Password" />
+         <label>
+            <Field type="checkbox" name="agree-tos"/>
+            <span>I agree with the Terms of Service</span>
+         </label>
+         <button type="submit">Submit</button>
+      </Form>
    );
 }
 
-export default UserForm;
+export default withFormik({})(UserForm);
