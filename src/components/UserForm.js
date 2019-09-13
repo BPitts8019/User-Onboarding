57c4893cbfdb675820945bb19eae597598757f84
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-12 18:07:51 -0700

    Added form validation

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,17 +1,32 @@
-import React from "react";
-import {withFormik, Form, Field} from "formik";
+import React, {useEffect} from "react";
+import {withFormik, Form, Field, ErrorMessage} from "formik";
+import * as yup from "yup";
 import axios from "axios";
 
-function UserForm (props) {
+function UserForm ({errors}) {
+   // useEffect(() => {
+   //    if (errors && Object.keys(errors).length > 0) {
+   //       console.log(errors);
+   //    }
+   // });
+
+   function isError (field) {
+      return (errors[field])? true : false;
+   }
+
    return (
       <Form>
-         <Field type="text" name="name" placeholder="Name" />
-         <Field type="email" name="email" placeholder="Email" />
-         <Field type="password" name="password" placeholder="Password" />
+         <Field type="text" name="name" className={isError("name")? "error" : ""} placeholder="Name" />
+         <ErrorMessage name="name" className="error" component="p" />
+         <Field type="email" name="email" className={isError("name")? "error" : ""} placeholder="Email" />
+         <ErrorMessage name="email" className="error" component="p" />
+         <Field type="password" name="password" className={isError("name")? "error" : ""} placeholder="Password" />
+         <ErrorMessage name="password" className="error" component="p" />
          <label>
-            <Field type="checkbox" name="agreedTos"/>
+            <Field type="checkbox" name="agreedTos" className={isError("name")? "error" : ""} />
             <span>I agree with the Terms of Service</span>
          </label>
+         <ErrorMessage name="agreedTos" className="error" component="p" />
          <button type="submit">Submit</button>
       </Form>
    );
@@ -26,6 +41,22 @@ export default withFormik({
          agreedTos: values.agreedTos || false
       };
    },
+   validationSchema: yup.object().shape({
+      name: yup
+         .string("Your name must be a string value.")
+         .required("Please enter your name."),
+      email: yup
+         .string()
+         .email("The email address entered is invalid. Try again")
+         .required("Please enter an email address."),
+      password: yup
+         .string()
+         .min(6, "You passoword must be at least 6 characters long.")
+         .required("Please enter a password."),
+      agreedTos: yup
+         .boolean()
+         .oneOf([true], "You must agree to the Terms of Service.")
+   }),
    handleSubmit: (formData) => {
       axios.post("https://reqres.in/api/users", formData)
          .then(response => {
