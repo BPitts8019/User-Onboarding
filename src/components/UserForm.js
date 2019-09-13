6756111a65c71f5f5e6ae2866b7a459db5bf9590
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-12 17:06:21 -0700

    Form submits to ReqRes REST API and receives a response

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {withFormik, Form, Field} from "formik";
+import axios from "axios";
 
 function UserForm (props) {
    return (
@@ -8,7 +9,7 @@ function UserForm (props) {
          <Field type="email" name="email" placeholder="Email" />
          <Field type="password" name="password" placeholder="Password" />
          <label>
-            <Field type="checkbox" name="agree-tos"/>
+            <Field type="checkbox" name="agreedTos"/>
             <span>I agree with the Terms of Service</span>
          </label>
          <button type="submit">Submit</button>
@@ -16,4 +17,22 @@ function UserForm (props) {
    );
 }
 
-export default withFormik({})(UserForm);
+export default withFormik({
+   mapPropsToValues: values => {
+      return {
+         name: values.name || "",
+         email: values.email || "",
+         password: values.password || "",
+         agreedTos: values.agreedTos || false
+      };
+   },
+   handleSubmit: (formData) => {
+      axios.post("https://reqres.in/api/users", formData)
+         .then(response => {
+            console.log(response);
+         })
+         .catch(error => {
+            console.error(error);
+         });
+   }
+})(UserForm);
